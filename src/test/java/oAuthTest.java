5a9641da10b62a7e498dc3bf1465c101723c3cd5
Serdar <serdar2225@gmail.com>
2020-10-06 09:31:15 -0400

    selenium and chrome driver

--- src/test/java/oAuthTest.java
+++ src/test/java/oAuthTest.java
@@ -1,4 +1,6 @@
 import io.restassured.path.json.JsonPath;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 
 import static io.restassured.RestAssured.*;
 import static org.hamcrest.MatcherAssert.assertThat;
@@ -6,6 +8,11 @@ import static org.hamcrest.Matchers.*;
 
 public class oAuthTest {
     public static void main(String[] args) {
+        System.setProperty("webdriver.chrome.driver", "C:\\Users\\suler\\Desktop\\Selenium\\chromedriver\\chromedriver.exe");
+        WebDriver driver;
+        driver = new ChromeDriver();
+
+        driver.get("http://www.dhtmlgoodies.com/scripts/drag-drop-custom/demo-drag-drop-3.html");
 
         String accessTokenResponse = given().queryParams("code","4%2F4wHmv9gggwV3ICXDzJdsT6sn2Kvi6HcmPENUaEO5b0MH1U37nO1nKhhFMwCIXYMvKR67ymHF1lnaD4SW_YMduao").
                 queryParams("client_id","692183103107-p0m7ent2hk7suguv4vq22hjcfhcr43pj.apps.googleusercontent.com").
