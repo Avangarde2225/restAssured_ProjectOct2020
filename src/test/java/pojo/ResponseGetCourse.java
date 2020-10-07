a077af7a8a32112170940075d161f2463d6a5cbc
Serdar <serdar2225@gmail.com>
2020-10-07 07:24:09 -0400

    pojo implementation

---
+++ src/test/java/pojo/ResponseGetCourse.java
@@ -0,0 +1,62 @@
+package pojo;
+
+public class ResponseGetCourse {
+
+    private String url;
+    private String services;
+    private String expertise;
+    private String courses;
+    private String intructor;
+    private String linkedin;
+
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+
+    public String getServices() {
+        return services;
+    }
+
+    public void setServices(String services) {
+        this.services = services;
+    }
+
+    public String getExpertise() {
+        return expertise;
+    }
+
+    public void setExpertise(String expertise) {
+        this.expertise = expertise;
+    }
+
+    public String getCourses() {
+        return courses;
+    }
+
+    public void setCourses(String courses) {
+        this.courses = courses;
+    }
+
+    public String getIntructor() {
+        return intructor;
+    }
+
+    public void setIntructor(String intructor) {
+        this.intructor = intructor;
+    }
+
+    public String getLinkedin() {
+        return linkedin;
+    }
+
+    public void setLinkedin(String linkedin) {
+        this.linkedin = linkedin;
+    }
+
+
+}
